2e01fee73c8c251954cd44bca4ee0a2cd875806d
Nityam24 <nityampatel2402@gmail.com>
2023-10-28 22:44:41 +0530

    update

---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+function NavBar() {
+  return (
+    <nav>
+      <Link href={"/"}>Todo List</Link>
+      <Link></Link>
+    </nav>
+  );
+}
+
+export default NavBar;
