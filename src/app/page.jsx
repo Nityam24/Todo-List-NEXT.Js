672ab735b09a18ec231234daf129f794030aa3af
Nityam24 <nityampatel2402@gmail.com>
2023-10-30 23:33:27 +0530

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import TopicList from "./components/TopicList";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <TopicList />
+    </main>
+  );
 }
