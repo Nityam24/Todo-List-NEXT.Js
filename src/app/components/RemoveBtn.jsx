dd476a6895617ecf567b68f211f19788f6d0eea4
Nityam24 <nityampatel2402@gmail.com>
2023-10-30 23:49:04 +0530

    update

---
+++ src/app/components/RemoveBtn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HiOutlineTrash } from "react-icons/hi";
+
+function RemoveBtn() {
+  return (
+    <button className="text-red-400">
+      <HiOutlineTrash size={24}></HiOutlineTrash>
+    </button>
+  );
+}
+
+export default RemoveBtn;
