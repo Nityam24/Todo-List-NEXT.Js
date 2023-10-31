eba9cd5c524833199fcbaca97c21ec1a27612554
Nityam24 <nityampatel2402@gmail.com>
2023-10-31 15:15:52 +0530

    update

--- src/app/addTopic/page.jsx
+++ src/app/addTopic/page.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function AddTopic() {
-  return <div> AddTopic</div>;
+  return (
+    <form className="flex flex-col gap-3">
+      <input
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic title"
+      />
+
+      <input
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Description"
+      />
+
+      <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
+        Add Topic
+      </button>
+    </form>
+  );
 }
 
 export default AddTopic;
--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <div className="max-w-3xl mx -auto p-4">
           <NavBar />
-          {children}
+          <div className="mt-8">{children}</div>
         </div>
       </body>
     </html>
