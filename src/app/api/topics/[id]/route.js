3610e385b52a74e029ff0ea4a96cf4830abafd27
Nityam24 <nityampatel2402@gmail.com>
2023-11-11 16:54:01 +0530

    update:GET

--- src/app/api/topics/[id]/route.js
+++ src/app/api/topics/[id]/route.js
@@ -3,8 +3,15 @@ import Topic from "@/models/topic";
 import { NextResponse } from "next/server";
 export async function PUT(request, { params }) {
   const { id } = params;
-  const { newtitle: title, newDescription: description } = request.json();
+  const { newtitle: title, newDescription: description } = await request.json();
   await connectMonogoDB();
   await Topic.findByIdAndUpdate(id, { title, description });
   return NextResponse.json({ message: "Topic Updated" }, { status: 200 });
+}
+
+export async function GET(request, { params }) {
+  const { id } = params;
+  await connectMonogoDB();
+  const topic = await Topic.findOne({ _id: id });
+  return NextResponse.json({ Topic }, { status: 200 });
 }
