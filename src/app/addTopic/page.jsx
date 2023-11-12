ad1bcfbf93cdd7383eb58e7fdd05ddbb709e833e
Nityam24 <nityampatel2402@gmail.com>
2023-11-12 19:22:19 +0530

    update:addTopic

--- src/app/addTopic/page.jsx
+++ src/app/addTopic/page.jsx
@@ -1,6 +1,40 @@
-import React from "react";
+"use clent";
+
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 function AddTopic() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !description) {
+      alert("Title and Description are required.");
+      return;
+    }
+    try {
+      const res = await fetch("http://localhost:3000/api/topics", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ title, description }),
+      });
+
+      if (res.ok) {
+        router.push("/");
+      } else {
+        throw new Error("Failed to create a topic");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <form className="flex flex-col gap-3">
       <input
