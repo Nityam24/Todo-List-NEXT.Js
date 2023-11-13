066e883f190161d6729e1e6cab06f397251c59dc
Nityam24 <nityampatel2402@gmail.com>
2023-11-13 17:00:42 +0530

    update

--- src/app/editTopic/[id]/page.jsx
+++ src/app/editTopic/[id]/page.jsx
@@ -1,10 +1,29 @@
 import EditTopicForm from "@/components/EditTopicForm";
 import React from "react";
 
-function EditTopic() {
+getTopicById = async () => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topic");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function EditTopic({ params }) {
+  const { id } = params;
+  const { topic } = await getTopicById(id);
+  const { title, description } = topic;
+  console.log("id:");
   return (
     <div>
-      <EditTopicForm />
+      <EditTopicForm id={id} title={title} description={description} />
     </div>
   );
 }
