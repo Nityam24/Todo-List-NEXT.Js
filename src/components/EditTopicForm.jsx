4b545bc03d6737e8f914b9c2858ac995448d14ac
Nityam24 <nityampatel2402@gmail.com>
2023-11-13 17:22:43 +0530

    update

--- src/components/EditTopicForm.jsx
+++ src/components/EditTopicForm.jsx
@@ -1,18 +1,47 @@
 "use client";
 
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
-function EditTopicForm() {
+function EditTopicForm({ id, title, description }) {
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
+  const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ newTitle, newDescription }),
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to update topic");
+      }
+
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <form className="flex flex-col gap-3">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
       <input
         className="border border-slate-500 px-8 py-2"
+        onChange={(e) => setNewTitle(e.target.value)}
+        value={newTitle}
         type="text"
         placeholder="Topic title"
       />
 
       <input
         className="border border-slate-500 px-8 py-2"
+        onChange={(e) => setNewDescription(e.target.value)}
+        value={newDescription}
         type="text"
         placeholder="Topic Description"
       />
