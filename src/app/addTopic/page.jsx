0b9ea329d0767051f551bcc0bbb1dfb0a9ebc943
Nityam24 <nityampatel2402@gmail.com>
2023-11-13 16:30:25 +0530

    update

--- src/app/addTopic/page.jsx
+++ src/app/addTopic/page.jsx
@@ -36,7 +36,7 @@ function AddTopic() {
   };
 
   return (
-    <form className="flex flex-col gap-3">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
       <input
         className="border border-slate-500 px-8 py-2"
         onChange={(e) => setTitle(e.target.value)}
@@ -47,11 +47,16 @@ function AddTopic() {
 
       <input
         className="border border-slate-500 px-8 py-2"
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
         type="text"
         placeholder="Topic Description"
       />
 
-      <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit ">
+      <button
+        type="submit"
+        className="bg-green-600 font-bold text-white py-3 px-6 w-fit "
+      >
         Add Topic
       </button>
     </form>
